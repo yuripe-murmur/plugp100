5a8cabd1a0eb46b381df61417120f92b354fb9e9
yuripe-murmur <yuripeyamashita@gmail.com>
2022-01-05 12:43:30 +0800

    main()

--- plugp100/example.py
+++ plugp100/example.py
@@ -1,20 +1,18 @@
-import asyncio
+# import asyncio
 
 from plugp100 import TapoApiClient
 
 
-# async def main():
-#     # create generic tapo api
-#     sw = TapoApiClient("192.168.1.107", "yuripeyamashita@gmail.com", "tp21Yu861223")
-#     await sw.login()
-#     await sw.on()
-#     # await sw.set_brightness(100)
-#     # print(await sw.get_state())
+async def main():
+    # create generic tapo api
+    sw = TapoApiClient("192.168.1.107", "yuripeyamashita@gmail.com", "tp21Yu861223")
+    await sw.login()
+    await sw.on()
+    # await sw.set_brightness(100)
+    # print(await sw.get_state())
 #
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(main())
 # loop.run_until_complete(asyncio.sleep(0.1))
 # loop.close()
-sw = TapoApiClient("192.168.1.107", "yuripeyamashita@gmail.com", "tp21Yu861223")
-await sw.login()
-await sw.on()
+main()
